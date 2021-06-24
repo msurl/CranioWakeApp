26d3128bc8673833357ba9f0e58cfa467b284ba3
msurl <masur101@hhu.de>
2021-06-24 14:30:28 +0200

    added new factory method to dialogbuilder class

--- app/src/main/java/com/app/craniowake/view/activityHelper/CraniowakeDialogBuilder.java
+++ app/src/main/java/com/app/craniowake/view/activityHelper/CraniowakeDialogBuilder.java
@@ -3,16 +3,31 @@ package com.app.craniowake.view.activityHelper;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Application;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.app.craniowake.R;
+import com.app.craniowake.data.model.Complication;
+import com.app.craniowake.databinding.DialogComplicationBinding;
+import com.app.craniowake.functional.VoidFunction;
 import com.app.craniowake.generated.callback.OnClickListener;
+import com.app.craniowake.view.OperationActivity;
+import com.app.craniowake.view.viewModel.ComplicationViewModel;
 
 import java.util.function.Function;
 
+// TODO: Vielleicht lieber DialogFragment nutzen statt dieser Variante
 public class CraniowakeDialogBuilder {
 
     public static Dialog of(Context context, int layoutId, int buttonId) {
@@ -31,25 +46,8 @@ public class CraniowakeDialogBuilder {
         return dialog;
     }
 
-    public static Dialog withoutDismissOnButtonClick(Context context, int layoutId, int buttonId) {
-        Dialog dialog = new Dialog(context) {
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                this.setCancelable(false);
-                this.setContentView(layoutId);
-
-                Button closeButton = this.findViewById(buttonId);
-                closeButton.setOnClickListener(v -> this.dismiss());
-            }
-        };
-        return dialog;
-    }
-
-
-//    public static Dialog of(Activity activity, int layoutId, int buttonId, Function<Void, Void> afterDismiss) {
-//        Dialog dialog = new Dialog(activity) {
+//    public static Dialog withOnClickListener(Context context, int layoutId, int buttonId, OnClickListener onClickListener) {
+//        Dialog dialog = new Dialog(context) {
 //            @Override
 //            protected void onCreate(Bundle savedInstanceState) {
 //                super.onCreate(savedInstanceState);
@@ -58,12 +56,46 @@ public class CraniowakeDialogBuilder {
 //                this.setContentView(layoutId);
 //
 //                Button closeButton = this.findViewById(buttonId);
-//                closeButton.setOnClickListener(v -> {
-//                    this.dismiss();
-//                    afterDismiss.apply(null);
-//                });
+//                closeButton.setOnClickListener(onClickListener);
 //            }
 //        };
 //        return dialog;
 //    }
+
+    public static Dialog complicationDialogWithDataBinding(AppCompatActivity activity, Long operationId) {
+        final ComplicationViewModel viewModel = new ViewModelProvider(activity).get(ComplicationViewModel.class);
+        viewModel.setOperationId(operationId);
+        DialogComplicationBinding binding = DataBindingUtil.inflate(LayoutInflater.from(activity), R.layout.dialog_complication, null, false);
+        binding.setViewmodel(viewModel);
+
+        ComplicationDialog dialog = new ComplicationDialog(activity, viewModel::addComplication);
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(binding.getRoot());
+
+        return dialog;
+    }
+
+    private static class ComplicationDialog extends Dialog{
+
+        VoidFunction executeOnCloseButtonClick;
+
+        public ComplicationDialog(@NonNull Context context, VoidFunction function) {
+            super(context);
+
+            executeOnCloseButtonClick = function;
+        }
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Button closeButton = this.findViewById(R.id.btn_dialog_complication);
+            closeButton.setOnClickListener(v -> {
+                executeOnCloseButtonClick.execute();
+                super.dismiss();
+            });
+        }
+    }
 }
